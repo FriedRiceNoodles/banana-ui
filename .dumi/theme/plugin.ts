d406ab7474666836391f492b91c0d5129ea59135
BugMaker.Huang <BugMaker.Huang@1695529682694277.onaliyun.com>
2024-01-30 18:04:35 +0800

    feat(doc site): imported a theme

--- .dumi/theme/plugin.ts
+++
@@ -1,8 +0,0 @@
-import { IApi } from 'dumi';
-import {join} from "path"
-
-export default (api: IApi) => {
-  // api.addTmpGenerateWatcherPaths(() => join(__dirname,'../../node_modules/@banana'));
-  // console.log('>>>>>>>>>>>>>>>>', join(__dirname,'../../forceRender.md'));
-  // api.addTmpGenerateWatcherPaths(() => '../../forceRender.md')
-};
