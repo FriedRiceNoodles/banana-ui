2eaefb9cf962350cb8ae265e9630f65bb9fa982d
zia.huang <youniverse@yfs20220001.local>
2024-02-21 09:38:30 +0800

    chore(switch): complete switch doc

--- docs/example/Switch/demos/withContent.tsx
+++ docs/example/Switch/demos/withContent.tsx
@@ -8,10 +8,20 @@ import { Switch } from '@banana-ui/react';
 
 export default function WithContent() {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '6px',
+      }}
+    >
       <Switch>
         <span slot="checked">开启</span>
         <span slot="unchecked">关闭</span>
+      </Switch>
+      <Switch>
+        <span slot="checked">1</span>
+        <span slot="unchecked">0</span>
       </Switch>
     </div>
   );
