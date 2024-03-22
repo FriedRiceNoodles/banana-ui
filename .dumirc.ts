6d5d4722d383bd4ccd7e34325d1d5c24a8e6ee37
ZIA-Hans <1025013204@qq.com>
2024-03-22 17:06:28 +0800

    chore: remove header name and add marigin bottom in home page

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   logo: '/logos/nav-logo-375.png',
   themeConfig: {
-    name: 'Banana UI',
+    name: '',
     footer: false,
     hero: {
       showCustomContent: true,
